69d68f8df2bfa2170c3de204f35f5062c95c0d5c
吴垂雄 <55620073+ChuixiongWu@users.noreply.github.com>
2022-09-20 11:22:10 +0800

    fixed some error
    Modified the exception raising.

--- stabilizer.py
+++ stabilizer.py
@@ -12,7 +12,7 @@ class treenode:
         self.x = x
         self.y = y
         self.z = z
-        self.value = value   
+        self.value = value
     def no_branch(self):
         if self.i == None and self.x == None and self.y == None and self.z == None:
             return True
@@ -22,7 +22,7 @@ class treenode:
         if self.root is not None:
             return self.root
         else:
-            sys.exit('No root found!')
+            raise Exception('This node has no root.')
 
 class stabilizer:
     def __init__(self, p):
@@ -33,10 +33,12 @@ class stabilizer:
                 print('No such file! ---',p)
         else:
             self.init_qiskit(p)
+        self.build_tree_from_op()
         return
     def init_from_file(self, p):
         self.data = []
         self.operator = []
+        self.tree = None
         with open(p, 'r') as f:
             for line in iter(f):
                 if line == '':
@@ -49,9 +51,11 @@ class stabilizer:
     def init_qiskit(self, p):
         self.operator = p.primitive.paulis.to_labels()
         self.data = p.primitive.coeffs.real
+        self.tree = None
     def print_the_node(self, node):
         line = ''
         while node.rootpos is not None:
+            # print(node)
             line += node.rootpos
             node = node.root
         return line[::-1]
@@ -73,8 +77,7 @@ class stabilizer:
                 node.z = treenode(root=node, rootpos='Z')
             return node.z
         else:
-            print('Undefined Symbol!',thechar)
-            sys.exit()       
+            raise Exception('Undefined Symbol!',thechar)     
     def build_tree_from_op(self):
         self.tree = treenode()
         for i_idx, i in enumerate(self.operator):
@@ -82,6 +85,8 @@ class stabilizer:
             for j in i:
                 curnode = self.add_branch(curnode, j)
             curnode.value = self.data[i_idx]
+        self.energynode = self.get_node('I'*len(self.operator[0]))
+        self.treetype = self.tree.__class__
         return
     def get_str_from_tree(self, curnode, s, l):
         if len(s) < l:
@@ -98,12 +103,14 @@ class stabilizer:
             self.tmp_data.append(curnode.value)
             return
         return
-    def build_ops_from_tree(self):
-        self.tmp_opr = []
-        self.tmp_data = []
+    def flush_operators(self):
+        if type(self.tree) is None:
+            raise Exception('There is no self.tree! Cannot generate operators.')
+        self.operator = []
+        self.data = []
         curnode = self.tree
         self.get_str_from_tree(curnode, '', len(self.operator[0]))
-        return self.tmp_opr, self.tmp_data
+        return
     def get_noti(self, opr):
         notilist = []
         for j_idx, j in enumerate(opr):
@@ -140,8 +147,7 @@ class stabilizer:
                     i -= 1
                     break
             else:
-                print('Illegal char!', thechar)
-                sys.exit()
+                raise Exception('Illegal char!', thechar)
         if i == l-1:
             return True
         else:
@@ -150,8 +156,7 @@ class stabilizer:
         if array == '':
             return self.tree
         if check and self.is_term_exist(array) is False:
-            print('No such node!')
-            sys.exit()
+            raise Exception('No such node!')
         curnode = self.tree
         for j in array:
             if j == 'I':
@@ -163,25 +168,32 @@ class stabilizer:
             elif j == 'Z':
                 curnode = curnode.z
             else:
-                print('Undefined Symbol!',thechar)
-                sys.exit()
+                raise Exception('Undefined Symbol!',j)
         return curnode
     def n_add_2n(self, n1, n2, mp):
         # here the node is surely exist
         if n2.i is None:
+            if type(n1.i) == self.treetype:
+                n1.i.root = n2
             n2.i = n1.i
         elif n1.i is not None:
             # node2.i not empty, node1.i not empty -> node to node!
             self.n_add_2n(n1.i, n2.i, mp)
         if n2.x is None:
+            if type(n1.x) == self.treetype:
+                n1.x.root = n2
             n2.x = n1.x
         elif n1.x is not None:
             self.n_add_2n(n1.x, n2.x, mp)
         if n2.y is None:
+            if type(n1.y) == self.treetype:
+                n1.y.root = n2
             n2.y = n1.y
         elif n1.y is not None:
             self.n_add_2n(n1.y, n2.y, mp)
         if n2.z is None:
+            if type(n1.z) == self.treetype:
+                n1.z.root = n2
             n2.z = n1.z
         elif n1.z is not None:
             self.n_add_2n(n1.z, n2.z, mp)
@@ -211,6 +223,7 @@ class stabilizer:
         for i in foldarray:
             if self.is_term_exist(i):
                 print('Now change',i[:qbit[0]+1],'to',i[:qbit[0]]+tochar)
+                print('Choose',qbit[1],'to be',mp)
                 self.fold_node_to_node(i[:qbit[0]+1], i[:qbit[0]]+tochar, mp, check=True)
                 ngiveroot = self.get_node(i[:qbit[0]]) 
                 if qbit[1] == 'I':
@@ -222,12 +235,10 @@ class stabilizer:
                 elif qbit[1] == 'Z':
                     ngiveroot.z = None
                 else:
-                    print('No such symbol!', qbit[1])
-                    sys.exit()
+                    raise Exception('No such symbol!', qbit[1])
         return
     def run(self):
-        self.build_tree_from_op()
-        self.operator, self.data = self.build_ops_from_tree()
+        self.operator, self.data = self.flush_operators()
         print('system energy:', self.data[0])
         while len(self.operator) > 2:
             rank = np.argmax(np.abs(self.data[1:]))
@@ -241,12 +252,11 @@ class stabilizer:
                 print('Remaining', l,'terms')
                 for i in range(l):
                     print('%15.8f\t%s' %(self.data[i],self.operator[i]))
-                sys.exit()
+                raise Exception('Finish running.')
             else:
                 print('The leading term:', maxopr, maxvalue)
                 mp = 1 if self.get_node(maxopr).value < 0 else -1
-                print('Choose',qbits[0][1],'to be',mp)
                 self.treefold(qbits[0], 'I', mp)
-                self.operator, self.data = self.build_ops_from_tree()
+                self.operator, self.data = self.flush_operators()
                 print('system energy:', self.data[0])
         return
