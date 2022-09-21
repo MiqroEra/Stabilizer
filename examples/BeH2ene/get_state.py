8ec585f134d720ec4fbe288159cdc159538f82dc
吴垂雄 <55620073+ChuixiongWu@users.noreply.github.com>
2022-09-21 14:02:29 +0800

    Add files via upload

---
+++ examples/BeH2ene/get_state.py
@@ -0,0 +1,4 @@
+from stabilizer import hf
+
+myhf = hf('./sourcedata/BeH2-1.32.txt')
+myhf.get_state_energy('000010111101')
---
+++ examples/BeH2ene/run_stabilizer.py
@@ -0,0 +1,4 @@
+from stabilizer import hf
+
+myhf = hf('./sourcedata/BeH2-1.32.txt')
+myhf.run()
